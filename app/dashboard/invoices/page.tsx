b157512cbc6b7a9bd3c6f5a97b5845c61b0da364
Vrunda <vrunda1002@gmail.com>
2024-01-31 12:35:41 +0530

    Mutating data(crud)

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -27,7 +27,7 @@ export default async function Page({
       </div>
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
         <Search placeholder="Search invoices..." />
-        {/* <CreateInvoice /> */}
+        <CreateInvoice />
       </div>
       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <Table query={query} currentPage={currentPage} />
