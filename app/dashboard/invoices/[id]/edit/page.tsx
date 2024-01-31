bab3dec9c6006e94ede09882e154ab88268c323d
Vrunda <vrunda1002@gmail.com>
2024-01-31 12:51:54 +0530

    Error handling

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,7 +1,8 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
- 
+import { notFound } from 'next/navigation';
+
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
 
@@ -10,6 +11,10 @@ export default async function Page({ params }: { params: { id: string } }) {
     fetchCustomers(),
   ]);
 
+  if (!invoice) {
+    notFound();
+  }
+  
   return (
     <main>
       <Breadcrumbs
